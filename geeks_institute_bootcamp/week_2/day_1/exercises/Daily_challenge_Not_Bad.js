227f88291360e649d696b8c98a7a799c1abe0828
OUSSAMA NAYA <oussamanaya8@gmail.com>
2026-02-02 14:08:56 +0100

    add the Daily challenge: Play with words of day 1 week 2

--- geeks_institute_bootcamp/week_2/day_1/exercises/Daily_challenge_Not_Bad.js
+++ geeks_institute_bootcamp/week_2/day_1/exercises/Daily_challenge_Not_Bad.js
@@ -1,16 +1,66 @@
-let sentence = "The movie is not that bad, I like it";
-console.log("sentence : ", sentence);
+// 1st daily challenge
+function makeAllCaps(names)
+{
+  return new Promise((resolve, reject) => {
+    names.forEach(name => {
+        if( typeof name !== "string")
+        {
+            reject("Ooops");
+            return; // stoppe la fonction après reject          
+        }
+    });
 
-let wordNot = sentence.indexOf("not");
-console.log("Position of 'not':", wordNot);
+    names.forEach((name, index, arr) => {
+      arr[index] = name.toUpperCase();
+    });
+    resolve(names);
+  })
+}
 
-let wordBad = sentence.indexOf("bad");
-console.log("Position of 'bad':", wordBad);
+function sortWords(namesUpperCase)
+{
+  return new Promise((resolve, reject) => {
+    if(namesUpperCase.length > 4)
+    {
+      resolve(namesUpperCase.sort())
+    }
+    else
+    {
+      reject("the lenght of array less then 4.")
+    }
+  });
+}
 
-if (wordNot !== -1 && wordBad !== -1 && wordBad > wordNot) {
-  // Remplacer "not ... bad" par "good"
-  let newSentence = sentence.slice(0, wordNot) + "good" + sentence.slice(wordBad + 3);
-  console.log("Modified sentence:", newSentence);
-} else {
-  console.log("Sentence unchanged:", sentence);
+
+let names = ["Oussama", "Othman", "Mohammed", "Ali", "Iman", "Ahmmed"];
+
+makeAllCaps(names)
+  .then(result => {
+    console.log(result)
+    return sortWords(result); // retourne la Promise suivante
+  })
+  .then(sortedResult => {
+    console.log("Sorted:", sortedResult);
+  })
+  .catch(message => console.log(message));
+
+
+// sortWords(namesUpperCase)
+//   .then(result => console.log(result))
+//   .catch(message => console.log(message));
+
+// 2nd daily challenge
+function toJs() {
+  return new Promise((resolve, reject) => {
+    try {
+      const morseJS = JSON.parse(morse); // transforme la chaîne en objet
+      if (Object.keys(morseJS).length === 0) {
+        reject("Morse object is empty!");
+      } else {
+        resolve(morseJS);
+      }
+    } catch (error) {
+      reject("Invalid JSON!");
+    }
+  });
 }
