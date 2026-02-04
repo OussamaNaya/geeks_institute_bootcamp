0e004f0c0206f00c0cf4257e9a49210c51764d06
OUSSAMA NAYA <oussamanaya8@gmail.com>
2026-02-04 20:13:24 +0100

    add Exercises XP : Animations exercice for day 3 week 1

--- geeks_institute_bootcamp/week_1/day_3/exercises/exercises_xp_ninja.js
+++ geeks_institute_bootcamp/week_1/day_3/exercises/exercises_xp_ninja.js
@@ -1,9 +1,61 @@
-// ===== Exercise 1
+// // ===== Exercise 1
+// let containerDiv = document.getElementById("container");
+// let clearButton = document.getElementById("clear");
+// let counter = 0;
 
-// ===== Exercise 2
 
-// ===== Exercise 3
+// setTimeout(() => {
+//     alert("Hello World !")
+// } , 2000);
 
-// ===== Exercise 4
+// const timer = setInterval(addHelloWorld, 2000);
 
-// ===== Exercise ...
+// function addHelloWorld()
+// {
+//     if(counter >= 5){
+//         clearInterval(timer);
+//         return;
+//     }
+
+//     let newP = document.createElement("p");
+//     newP.textContent = "Hello World !";
+
+//     containerDiv.appendChild(newP);
+//     counter ++;
+// }
+
+// clearButton.addEventListener("click", () => {
+//     alert("Done");
+//     clearInterval(timer);
+// });
+
+// // ===== Exercise 2
+const button = document.getElementById("btn");
+const box = document.getElementById("animate");
+const container = document.getElementById("container");
+
+let interval;
+
+button.addEventListener("click", myMove);
+
+function myMove() {
+  let position = 0;
+
+  // sécurité : stop ancien interval si on reclique
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    if (position >= container.offsetWidth - box.offsetWidth) {
+      clearInterval(interval); // stop animation
+    } else {
+      position++;
+      box.style.left = position + "px";
+    }
+  }, 1);
+}
+
+// // ===== Exercise 3
+
+// // ===== Exercise 4
+
+// // ===== Exercise ...
