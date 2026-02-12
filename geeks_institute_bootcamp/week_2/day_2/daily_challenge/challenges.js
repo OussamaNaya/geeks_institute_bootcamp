a3d1164c153b638febcd421059e39817feefd512
OUSSAMA NAYA <oussamanaya8@gmail.com>
2026-02-12 19:24:38 +0100

    add the Daily challenge : Go Wildcats for the week 2 day 2

--- geeks_institute_bootcamp/week_2/day_2/daily_challenge/challenges.js
+++ geeks_institute_bootcamp/week_2/day_2/daily_challenge/challenges.js
@@ -1,16 +1,51 @@
-// ====== Daily Challenge 1
-const firstArr = [1, 4, 9, 16];
+// ====== Daily Challenge 1 : Go Wildcats
+const gameInfo = [
+ {
+   username: "john",
+   team: "red",
+   score: 5,
+   items: ["ball", "book", "pen"]
+ },
+ {
+   username: "becky",
+   team: "blue",
+   score: 10,
+   items: ["tape", "backpack", "pen"]
+ },
+ {
+   username: "susy",
+   team: "red",
+   score: 55,
+   items: ["ball", "eraser", "pen"]
+ },
+ {
+   username: "tyson",
+   team: "green",
+   score: 1,
+   items: ["book", "pen"]
+ },
+];
 
-const secondArr = firstArr.map((value, index, arr) => {
-    console.log(value); // each element
-    console.log(index); // each index
-    console.log(arr); // the firstArr (logged 4 times)
-    return value * index;
+
+const usernames = [];
+gameInfo.forEach(player => {
+  usernames.push(player.username + "!");
 });
 
-console.log(secondArr);
-//expected output : [ 0, 4, 18, 48 ]
+console.log(usernames);
 
-// ====== Daily Challenge 2
+const winners = [];
+gameInfo.forEach(player => {
+  if (player.score > 5) {
+    winners.push(player.username);
+  }
+});
 
-// ====== Daily Challenge 3
+console.log(winners);
+
+let totalScore = 0;
+gameInfo.forEach(player => {
+  totalScore += player.score;
+});
+
+console.log(totalScore);
