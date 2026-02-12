5ff3c579a3bbe225b577e26ae77e42cc7e861325
OUSSAMA NAYA <oussamanaya8@gmail.com>
2026-02-12 17:27:38 +0100

    add the Daily challenge: Groceries for the Week 2 day 1

--- geeks_institute_bootcamp/week_2/day_1/daily_challenge/challenges.js
+++ geeks_institute_bootcamp/week_2/day_1/daily_challenge/challenges.js
@@ -1,5 +1,47 @@
-// ====== Daily Challenge 1
+// ====== Daily Challenge  : Groceries
+let client = "John";
 
-// ====== Daily Challenge 2
+const groceries = {
+    fruits : ["pear", "apple", "banana"],
+    vegetables: ["tomatoes", "cucumber", "salad"],
+    totalPrice : "20$",
+    other : {
+        paid : true,
+        meansOfPayment : ["cash", "creditCard"]
+    }
+}
 
-// ====== Daily Challenge 3
+const displayGroceries = (groceries) => {
+    groceries.fruits.forEach(item => {
+        console.log(item)
+    });
+}
+
+const cloneGroceries = () => {
+    let user = client;
+    console.log(`user : ${user}`);
+    console.log(`client : ${client}`);
+
+    client = "Betty";
+    console.log(`user : ${user}`);
+    console.log(`client : ${client}`);
+    // Response => Passage by value
+
+    let shopping = groceries;
+    console.log(`shopping : ${shopping.totalPrice}`);
+    console.log(`groceries : ${groceries.totalPrice}`);
+    // Response => Passage by reference
+
+    shopping.totalPrice = "35$";
+    console.log(`shopping : ${shopping.totalPrice}`);
+    console.log(`groceries : ${groceries.totalPrice}`);
+    // Response => Passage by reference 
+    
+    shopping.other.paid = false;
+    console.log(`shopping : ${shopping.other.paid}`);
+    console.log(`groceries : ${groceries.other.paid}`); 
+    // Response => Passage by reference
+}
+
+displayGroceries(groceries);
+cloneGroceries(); 
