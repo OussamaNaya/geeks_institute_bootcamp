664ded9ad565056c6a07d0238f16dba9af300632
OUSSAMA NAYA <oussamanaya8@gmail.com>
2026-02-12 18:58:48 +0100

    add the Exercises XP for the week 2 day 2

--- geeks_institute_bootcamp/week_2/day_2/daily_challenge/challenges.js
+++ geeks_institute_bootcamp/week_2/day_2/daily_challenge/challenges.js
@@ -1,4 +1,15 @@
 // ====== Daily Challenge 1
+const firstArr = [1, 4, 9, 16];
+
+const secondArr = firstArr.map((value, index, arr) => {
+    console.log(value); // each element
+    console.log(index); // each index
+    console.log(arr); // the firstArr (logged 4 times)
+    return value * index;
+});
+
+console.log(secondArr);
+//expected output : [ 0, 4, 18, 48 ]
 
 // ====== Daily Challenge 2
 
--- geeks_institute_bootcamp/week_2/day_2/exercises/exercises_xp.js
+++ geeks_institute_bootcamp/week_2/day_2/exercises/exercises_xp.js
@@ -1,50 +1,95 @@
-// ===== Exercise 1
-// function compareToTen(num){
-//     return new Promise((resolve, reject) => {
-//         if(num >= 10)
-//         {
-//             resolve("Number is greater than 10");
-//         }
-//         else
-//         {
-//             reject("Number is less than 10");
-//         }
-//     });
-// }
+// ===== Exercise 1 : Colors
+// const colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+// let isYeah = false;
 
-// compareToTen(15)
-//   .then(result => console.log(result))
-//   .catch(error => console.log(error))
-//    .finally( console.log("By by !"))
+// colors.forEach((item, index) => {
+//    if(item == "Violet")
+//    {
+//       console.log("Yeah");
+//       isYeah = true;
+//    }
+//    console.log(`${index+1}# choice is ${item}`);
+// });
 
-// compareToTen(8)
-//   .then(result => console.log(result))
-//   .catch(error => console.log(error))
+// if(!isYeah)
+//    console.log("No...");
 
 
+// ===== Exercise 2 : Colors #2
+// const colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+// const ordinal = ["th","st","nd","rd"];
 
-   // ===== Exercise 2
-// function success() {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       resolve("success");
-//     }, 4000);
-//   });
-// }
-
-// success().then(result => console.log(result));
+// colors.forEach((color, index) => {
+//   const position = index + 1;
+//   const suffix = position <= 3 ? ordinal[position] : ordinal[0];
+//   console.log(`${position}${suffix} choice is ${color}.`);
+// });
 
 
-   // ===== Exercise 3
-// Promise qui se résout avec la valeur 3
-let promiseResolve = Promise.resolve(3);
+// ===== Exercise 3 : Analyzing
+// ------1------
+// const fruits = ["apple", "orange"];
+// const vegetables = ["carrot", "potato"];
 
-promiseResolve
-  .then(result => console.log(result));
+// const result = ['bread', ...vegetables, 'chicken', ...fruits];
+// console.log(result);
+// // result => ['bread', 'carrot', 'potato', 'chicken', 'apple', 'orange']
+
+// // ------2------
+// const country = "USA";
+// console.log([...country]);
+// // result => ['U', 'S', 'A']
+
+// // ------Bonus------
+// let newArray = [...[,,]];
+// console.log(newArray);
+// // result => [undefined, undefined]
 
 
-// Promise qui se rejette avec "Boo!"
-let promiseReject = Promise.reject("Boo!");
+// ===== Exercise 4 : Employees
+// const users = [{ firstName: 'Bradley', lastName: 'Bouley', role: 'Full Stack Resident' },
+//              { firstName: 'Chloe', lastName: 'Alnaji', role: 'Full Stack Resident' },
+//              { firstName: 'Jonathan', lastName: 'Baughn', role: 'Enterprise Instructor' },
+//              { firstName: 'Michael', lastName: 'Herman', role: 'Lead Instructor' },
+//              { firstName: 'Robert', lastName: 'Hajek', role: 'Full Stack Resident' },
+//              { firstName: 'Wes', lastName: 'Reid', role: 'Instructor'},
+//              { firstName: 'Zach', lastName: 'Klabunde', role: 'Instructor'}];
 
-promiseReject
-  .catch(error => console.log(error));
+// // const welcomeStudents = 
+// const welcomeStudents = users.map((item, index) => {
+//    return `Hello ${item.firstName}`;
+// })
+
+// console.log(welcomeStudents);
+
+// const fullStackLastNames = users
+//    .filter((item) => item.role === 'Full Stack Resident')
+//    .map((item) => item.lastName);
+   
+// console.log(fullStackLastNames);
+
+
+// ===== Exercise 5 : Star Wars
+// const epic = ['a', 'long', 'time', 'ago', 'in a', 'galaxy', 'far far', 'away'];
+
+// const message = epic.reduce((accumulator, currentValue) => {
+//   return accumulator + ' ' + currentValue;
+// });
+
+// console.log(message);
+
+// ===== Exercise 6 :  Employees #2
+const students = [
+  {name: "Ray", course: "Computer Science", isPassed: true}, 
+  {name: "Liam", course: "Computer Science", isPassed: false}, 
+  {name: "Jenner", course: "Information Technology", isPassed: true}, 
+  {name: "Marco", course: "Robotics", isPassed: true}, 
+  {name: "Kimberly", course: "Artificial Intelligence", isPassed: false}, 
+  {name: "Jamie", course: "Big Data", isPassed: false}
+];
+
+students
+  .filter(student => student.isPassed)
+  .forEach(student => {
+    console.log(`Good job ${student.name}, you passed the course in ${student.course}`);
+  });
