f12851aa46e9d0657d680b18f8333e0e901f8a0a
OUSSAMA NAYA <oussamanaya8@gmail.com>
2026-02-16 15:55:48 +0100

    add the Exercises XP Gold for the week 2 day 3

--- geeks_institute_bootcamp/week_2/day_3/exercises/exercises_xp_gold.js
+++ geeks_institute_bootcamp/week_2/day_3/exercises/exercises_xp_gold.js
@@ -1,46 +1,41 @@
-// ===== Exercise 1 : Analyzing the map method
-// let l = [1, 2, 3].map(num => {
-//   if (typeof num === 'number') return num * 2;
-//   return ;
-// });
+// ===== Exercise 1 : print Full Name
+// function printFullName(obj){
+//     return `Your full name is ${obj.first} ${obj.last}`
+// }
 
-// console.log(l);  // [2, 4, 6]
+// printFullName({first: 'Elie', last:'Schoppik'}) 
 
+// ===== Exercise 2 : keys and values
+// function keysAndValues(obj) {
+//   // 1. Récupérer les clés et les trier
+//   const keys = Object.keys(obj).sort();
 
-// ===== Exercise 2 : Analyzing the reduce method
-// let l = [[0, 1], [2, 3]].reduce(
-//   (acc, cur) => {
-//     return acc.concat(cur);
-//   },
-//   [1, 2],
-// );
+//   // 2. Récupérer les valeurs dans le même ordre que les clés
+//   const values = keys.map(key => obj[key]);
 
-// console.log(l); //[1, 2, 0, 1, 2, 3]
+//   // 3. Retourner les deux tableaux
+//   return [keys, values];
+// }
 
-// ===== Exercise 3
-// const arrayNum = [1, 2, 4, 5, 8, 9];
-// const newArray = arrayNum.map((num, i) => {
-//     console.log(num, i);
-//     alert(num);
-//     return num * 2;
-// })
-// console.log(i); // error : not defind
+// console.log(keysAndValues({ a: 1, b: 2, c: 3 }));
 
+// ===== Exercise 3 : Counter class
+class Counter {
+  constructor() {
+    this.count = 0;
+  }
 
-// ===== Exercise 4 : Nested arrays
-const array = [[1],[2],[3],[[[4]]],[[[5]]]]
-let l = array.flat(2);
-array.forEach(element => {
-    console.log(element)  
-});
+  increment() {
+    this.count++;
+  }
+}
 
-const greeting = [["Hello", "young", "grasshopper!"], ["you", "are"], ["learning", "fast!"]];
-const result = greeting.map(arr => arr.join(" "));
-console.log(result);
+const counterOne = new Counter();
+counterOne.increment();
+counterOne.increment();
 
-const finalString = greeting.map(arr => arr.join(" ")).join(" ");
-console.log(finalString);
+const counterTwo = counterOne;
+counterTwo.increment();
 
-
-const trapped = [[[[[[[[[[[[[[[[[[[[[[[[[[3]]]]]]]]]]]]]]]]]]]]]]]]]]
-console.log(trapped.flat(25))
+console.log(counterOne.count);
+// Output => 3
