59f27eaee928ef64896f79a5780b3add0d2bf6fb
OUSSAMA NAYA <oussamanaya8@gmail.com>
2026-02-16 16:35:36 +0100

    add the Daily Challenge for the week 4 day 4

--- geeks_institute_bootcamp/week_4/day_4/daily_challenge/challenge_1.js
+++ geeks_institute_bootcamp/week_4/day_4/daily_challenge/challenge_1.js
@@ -1,1 +1,41 @@
+// 🏗️ Task 1: Inventory Setup
+// db.products.insertMany([
+//   {
+//     name: "Laptop",
+//     category: "Electronics",
+//     price: 1200,
+//     stock: 100
+//   },
+//   {
+//     name: "Smartphone",
+//     category: "Electronics",
+//     price: 800,
+//     stock: 25
+//   },
+//   {
+//     name: "Desk Lamp",
+//     category: "Home",
+//     price: 45,
+//     stock: 15
+//   },
+//   {
+//     name: "Notebook",
+//     category: "Stationery",
+//     price: 5,
+//     stock: 3
+//   },
+//   {
+//     name: "Pen",
+//     category: "Stationery",
+//     price: 2,
+//     stock: 50
+//   }
+// ]);
+
+// 📉 Task 2: The "Restock Needed" Alert
+// db.products.find(
+//   {
+//     price: { $lt: 10 }
+//   }
+// )
 
