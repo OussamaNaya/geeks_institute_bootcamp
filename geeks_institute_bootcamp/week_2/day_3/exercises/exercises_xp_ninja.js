df6ff22ef78ad311aa327865d386810da3b8f7de
OUSSAMA NAYA <oussamanaya8@gmail.com>
2026-02-16 16:00:42 +0100

    add the Exercises XP Ninja for the Week 2 day 3

--- geeks_institute_bootcamp/week_2/day_3/exercises/exercises_xp_ninja.js
+++ geeks_institute_bootcamp/week_2/day_3/exercises/exercises_xp_ninja.js
@@ -1,90 +1,21 @@
-// ===== Exercise 1 : Dog age to Human years
-// const data = [
-//   {
-//     name: 'Butters',
-//     age: 3,
-//     type: 'dog'
-//   },
-//    {
-//     name: 'Cuty',
-//     age: 5,
-//     type: 'rabbit'
-//   },
-//   {
-//     name: 'Lizzy',
-//     age: 6,
-//     type: 'dog'
-//   },
-//   {
-//     name: 'Red',
-//     age: 1,
-//     type: 'cat'
-//   },
-//   {
-//     name: 'Joey',
-//     age: 3,
-//     type: 'dog'
-//   },
-//   {
-//     name: 'Rex',
-//     age: 10,
-//     type: 'dog'
-//   },
-// ];
-
-// let age = 0;
-
-// data.reduce((acc, item) => {
-//     acc += item.age;
-// })
-// console.log(age);
-
-// age *= 7;
-// console.log(age);
-
-
-// ===== Exercise 2 : Email
-// const userEmail3 = ' cannotfillemailformcorrectly@gmail.com '
-// console.log(userEmail3.trim())
-
-
-// ===== Exercise 3 : Employees #3
-// const users = [{ firstName: 'Bradley', lastName: 'Bouley', role: 'Full Stack Resident' },
-//              { firstName: 'Chloe', lastName: 'Alnaji', role: 'Full Stack Resident' },
-//              { firstName: 'Jonathan', lastName: 'Baughn', role: 'Enterprise Instructor' },
-//              { firstName: 'Michael', lastName: 'Herman', role: 'Lead Instructor' },
-//              { firstName: 'Robert', lastName: 'Hajek', role: 'Full Stack Resident' },
-//              { firstName: 'Wes', lastName: 'Reid', role: 'Instructor'},
-//              { firstName: 'Zach', lastName: 'Klabunde', role: 'Instructor'}];
-
-// const usersObject = {};
-// for (let user of users) {
-//   const fullName = `${user.firstName} ${user.lastName}`;
-//   usersObject[fullName] = user.role;
-// }
-
-// console.log(usersObject);
-
-
-// ===== Exercise 4 : Array to Object
-// const letters = ['x', 'y', 'z', 'z'];
-
-// const letterCount = {};
-// for (let letter of letters) {
-//   if (letterCount[letter]) {
-//     letterCount[letter]++;
-//   } else {
-//     letterCount[letter] = 1;
+// // ===== Exercise 1 : Bird class
+// class Bird {
+//   constructor() {
+//     console.log("I'm a bird. 🦢");
 //   }
 // }
 
-// console.log(letterCount);
+// class Flamingo extends Bird {
+//   constructor() {
+//     console.log("I'm pink. 🌸");
+//     super();
+//   }
+// }
 
-// const letters = ['x', 'y', 'z', 'z'];
+// const pet = new Flamingo();
 
-// const letterCount = letters.reduce((obj, letter) => {
-//   obj[letter] = (obj[letter] || 0) + 1;
-//   return obj;
-// }, {});
-
-// console.log(letterCount);
+// Output => """
+// I'm pink. 🌸
+// exercises_xp_ninja.js:10
+// I'm a bird. 🦢
+// """
