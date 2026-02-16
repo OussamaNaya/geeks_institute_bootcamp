84f2ad76f323a7c71e1d88f8c9d251081d75cbd4
OUSSAMA NAYA <oussamanaya8@gmail.com>
2026-02-16 16:23:38 +0100

    add the Exercice XP for the Week 4 day 4

--- geeks_institute_bootcamp/week_4/day_4/exercises/exercises_xp.js
+++ geeks_institute_bootcamp/week_4/day_4/exercises/exercises_xp.js
@@ -1,9 +1,18 @@
-// ===== Exercise 1
+// // ===== Exercise 1
+// db.students.insertMany([
+//   { name: "Yassine", age: 19, city: "Rabat", major: "AI" },
+//   { name: "Sophia", age: 26, city: "Paris", major: "Cybersecurity" },
+//   { name: "Kaito", age: 22, city: "Tokyo", major: "Web Dev" },
+//   { name: "Elena", age: 31, city: "Madrid", major: "Data Science" },
+//   { name: "Marcus", age: 24, city: "New York", major: "AI" }
+// ])
 
-// ===== Exercise 2
 
-// ===== Exercise 3
+// // ===== Exercise 2: Data Intelligence
+// // Task A: The "Young Pros" Filter
+// // {age: { $gt: 18, $lt: 25}}
+// // Task C: The Graduation
+// // db.students.deleteOne({name: "Sophia"});
 
-// ===== Exercise 4
-
-// ===== Exercise ...
+// // 🏆 Boss Level Challenge (Optional)
+// // db.students.find().pretty()
