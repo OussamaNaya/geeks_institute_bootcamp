905e5ea8b1a801c2368aae89a1ec2ad5353ce25d
OUSSAMA NAYA <oussamanaya8@gmail.com>
2026-02-19 11:26:24 +0100

    add same execice of the cour for the week 3 day 2

---
+++ geeks_institute_bootcamp/week_3/day_2/exercises/test.js
@@ -0,0 +1,65 @@
+// console.log(fetch("https://api.artic.edu/api/v1/artworks/4"))
+
+
+// console.log("Starting ...")
+
+
+// const getArtwork = () => {
+//     console.log("Working ...")
+//     fetch("https://api.artic.edu/api/v1/artworks/14572")
+//         .then((response) => {
+//             if(response.ok){
+//                 return response.json()
+//             } else {
+//                 throw new Error("Wrong artwork")
+//             }
+//         })
+//         .then((obj) => {
+//             console.log(obj);
+//             console.log(`The painting is named ${obj.data.title} 
+//                          by the artist ${obj.data.artist_title}`)
+//         })
+//         .catch(function (error) {
+//             console.log(`We got the error ${error}`)
+//         });
+//     console.log("Work Done ...")
+// }
+
+
+// getArtwork()
+
+console.log("Starting ...")
+
+const data = {
+    title: "Article on Javascript",
+    body: "This is an article presenting the new features of Javascript", 
+    userId:1
+}
+
+const objBody = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {"Content-type": "application/json; charset=UTF-8"}
+}
+
+const addArticle = () => {
+    console.log("Working ...")
+    fetch("https://jsonplaceholder.typicode.com/posts", objBody)
+        .then((response) => {
+            console.log(response);
+            if(response.ok === true){
+                return response.json()
+            } else {
+                throw new Error("Wrong post")
+            }
+        })
+        .then((obj) => {
+            console.log(obj);
+        })
+        .catch((error)  => {
+            console.log(error);
+        });
+    console.log("Work Done ...")
+}
+
+addArticle()
