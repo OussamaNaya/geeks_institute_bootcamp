b5368befb08efba82405dba0095046b3c4dc1360
OUSSAMA NAYA <oussamanaya8@gmail.com>
2026-02-23 12:21:52 +0100

    add the exiercice 2, 3 of the Exercises XP Gold for the week 3 day 2

--- geeks_institute_bootcamp/week_3/day_2/exercises/exercises_xp_gold.js
+++ geeks_institute_bootcamp/week_3/day_2/exercises/exercises_xp_gold.js
@@ -1,46 +1,116 @@
-// ===== Exercise 1 : Giphy API #2
-const API_key = "hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My";
-const API_Griphy = "https://api.giphy.com/v1/gifs/search";
+// // ===== Exercise 1 : Giphy API #2
+// const API_key = "hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My";
+// const API_Griphy = "https://api.giphy.com/v1/gifs/search";
 
-let gifsSection = document.getElementById("gifs");
-let gifImg = document.getElementById("gifImg");
+// let gifsSection = document.getElementById("gifs");
+// let gifImg = document.getElementById("gifImg");
 
-fetch(`${API_Griphy}?q=sun&limit=1&offset=1&api_key=${API_key}`)
-    .then((response) => {
-        if(response.ok)
-        {
-            return response.json();
-        }
-        else
-        {
-            throw new Error("Wrong artwork");
-        }
-    })
-    .then((obj) => {
-        console.log(obj);
+// fetch(`${API_Griphy}?q=sun&limit=1&offset=1&api_key=${API_key}`)
+//     .then((response) => {
+//         if(response.ok)
+//         {
+//             return response.json();
+//         }
+//         else
+//         {
+//             throw new Error("Wrong artwork");
+//         }
+//     })
+//     .then((obj) => {
+//         console.log(obj);
 
-        let objGif = document.createElement("div");
-        objGif.textContent  = obj;
+//         let objGif = document.createElement("div");
+//         objGif.textContent  = obj;
 
-        const firstGif = obj.data[0];
-        console.log(`firstGif: `);
-        console.log(firstGif);
-        console.log("URL:", firstGif.images.fixed_height.url);
+//         const firstGif = obj.data[0];
+//         console.log(`firstGif: `);
+//         console.log(firstGif);
+//         console.log("URL:", firstGif.images.fixed_height.url);
 
-        gifImg.id = firstGif.id;
-        gifImg.src = firstGif.images.fixed_height.url;
+//         gifImg.id = firstGif.id;
+//         gifImg.src = firstGif.images.fixed_height.url;
 
-        gifsSection.appendChild(objGif);
-    })
-    .catch((err) => {
-        console.log(`catch: ${err}`);
-    })
+//         gifsSection.appendChild(objGif);
+//     })
+//     .catch((err) => {
+//         console.log(`catch: ${err}`);
+//     })
 
 
-// ===== Exercise 2
+// // ===== Exercise 2 : Analyze #2
+// let resolveAfter2Seconds = function () {
+//     console.log("starting slow promise");
+//     return new Promise(resolve => {
+//         setTimeout(function () {
+//             resolve("slow");
+//             console.log("slow promise is done");
+//         }, 2000);
+//     });
+// };
 
-// ===== Exercise 3
+// let resolveAfter1Second = function () {
+//     console.log("starting fast promise");
+//     return new Promise(resolve => {
+//         setTimeout(function () {
+//             resolve("fast");
+//             console.log("fast promise is done");
+//         }, 1000);
+//     });
+// };
 
-// ===== Exercise 4
+// let sequentialStart = async function () {
+//     console.log('==SEQUENTIAL START==');
+//     const slow = await resolveAfter2Seconds();
+//     console.log(slow);
+//     const fast = await resolveAfter1Second();
+//     console.log(fast);
+// }
 
-// ===== Exercise ...
+// sequentialStart()
+
+// Output => """
+// ==SEQUENTIAL START==
+// starting slow promise
+// slow promise is done
+// slow
+// starting fast promise
+// fast promise is done
+// fast
+// """
+
+
+// // ===== Exercise 3: Analyze #3
+// let resolveAfter2Seconds = function () {
+//     console.log("starting slow promise");
+//     return new Promise(resolve => {
+//         setTimeout(function () {
+//             resolve("slow");
+//             console.log("slow promise is done");
+//         }, 2000);
+//     });
+// };
+
+// let resolveAfter1Second = function () {
+//     console.log("starting fast promise");
+//     return new Promise(resolve => {
+//         setTimeout(function () {
+//             resolve("fast");
+//             console.log("fast promise is done");
+//         }, 1000);
+//     });
+// };
+
+// let concurrentStart = async function () {
+//     console.log('==CONCURRENT START with await==');
+//     const slow = resolveAfter2Seconds();
+//     const fast = resolveAfter1Second();
+//     console.log(await slow);
+//     console.log(await fast);
+// }
+
+// setTimeout(concurrentStart, 4000)
+
+
+// // ===== Exercise 4
+
+// // ===== Exercise ...
