1683aef556058a1846c9cbb1c83f6b420c0537ef
OUSSAMA NAYA <oussamanaya8@gmail.com>
2026-03-05 17:19:34 +0100

    add the schima student of the exercice of the day 5 week 4

--- geeks_institute_bootcamp/week_4/day_5/First Backend Bridge/app.js
+++ geeks_institute_bootcamp/week_4/day_5/First Backend Bridge/app.js
@@ -1,13 +1,32 @@
 const express = require("express")
+const mongoose = require('mongoose');
+
 
 const app = express();
 
-const mongoose = require('mongoose');
 
 // The Connection Handshake
 mongoose.connect('mongodb://localhost:27017/test')
   .then(() => console.log('✅ Connection Successful! Node.js is talking to MongoDB.'))
   .catch(err => console.error('❌ Connection Failed:', err));
+
+const studentSchema = new mongoose.Schema({
+  name: { 
+    type: String, 
+    required: [true, 'Name is mandatory'] 
+  },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  age: { 
+    type: Number, 
+    min: [18, 'Students must be at least 18 years old'] 
+  }
+});
+
+const Student = mongoose.model('Student', studentSchema);
 
 
 const PORT = 3000;
