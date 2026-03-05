c18962e614abcfcdc0f3f19709e9c10d9957055c
OUSSAMA NAYA <oussamanaya8@gmail.com>
2026-03-05 17:59:38 +0100

    add the CRUD method to the TaskMaster project for the day 5 week 4

--- geeks_institute_bootcamp/week_4/day_5/TaskMaster/app.js
+++ geeks_institute_bootcamp/week_4/day_5/TaskMaster/app.js
@@ -5,6 +5,40 @@ const connect = require("./db/dbConnection");
 const app = express();
 connect();
 
+const createTask = async () => {
+  try {
+    const newTask = await Task.create({
+      title: "Learn Mongoose Validation",
+      description: "Complete the daily challenge for the MongoDB bootcamp",
+    });
+    console.log("✅ Task Created:", newTask);
+  } catch (err) {
+    console.error("❌ Error:", err.message);
+    console.error("Error code:", err.code); // souvent 121 si validation MongoDB
+    console.dir(err.errInfo, { depth: null });}
+};
+
+console.log("createTask: ");
+// createTask()
+
+const getAllTasks = async () => {
+  const tasks = await Task.find();
+  console.log("📋 Current Tasks:", tasks);
+};
+
+getAllTasks();
+
+const completeTask = async (taskId) => {
+    try {
+        await Task.findByIdAndUpdate(taskId, { status: 'completed' });
+        console.log("🚀 Task marked as completed!");
+    } catch (error) {
+        consol.log(`error: ${error}`);
+    }
+};
+
+completeTask('69a9b48ebe794a80c4913c8b');
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
