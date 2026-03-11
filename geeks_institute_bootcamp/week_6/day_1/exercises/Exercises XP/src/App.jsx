0a5ade7b7fd19b4a1bc672420a57ba2f9e68fb23
OUSSAMA NAYA <oussamanaya8@gmail.com>
2026-03-11 15:33:46 +0100

    impliment the Exercise 2 : Object, for the Exercises XP of the day 1 week 7

--- geeks_institute_bootcamp/week_6/day_1/exercises/Exercises XP/src/App.jsx
+++ geeks_institute_bootcamp/week_6/day_1/exercises/Exercises XP/src/App.jsx
@@ -2,16 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserFavoriteAnimals from './UserFavoriteAnimals'
 
 function App() {
   const myelement = <h1>I Love JSX!</h1>;
   const sum = 5+5;
+
+  const user = {
+    firstName: 'Bob',
+    lastName: 'Dylan',
+    favAnimals : ['Horse','Turtle','Elephant','Monkey']
+  };
+
 
   return (
     <>
       Hello World!
       {myelement}
       React is {sum} times better with JSX
+
+      <h3>Hello Mr {user.lastName} {user.firstName}</h3>
+      <UserFavoriteAnimals favAnimals={user.favAnimals} />
     </>
   )
 }
