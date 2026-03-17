5a6cdebc03fe6f1b713fe640e53f72d0cbe44506
OUSSAMA NAYA <oussamanaya8@gmail.com>
2026-03-17 14:30:32 +0100

    Inpliment the "Part II : useState Hook" for the Exercises XP for the week 7 day 2

--- geeks_institute_bootcamp/week_7/day_2/Exercises XP/src/components/Car.jsx
+++ geeks_institute_bootcamp/week_7/day_2/Exercises XP/src/components/Car.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
+
 function Car(props) {
     const { name, model } = props;
+    const [color, setColor] = useState("red");
 
     return (
         <div>
-            <h1>This Car name it {name} and model it {model}</h1>
+            <h3>This Car name it {name} and model it {color} {model}</h3>
         </div>
     );
 }
