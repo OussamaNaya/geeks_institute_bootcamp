e08e1549c975877e38b2419a4c0f3e2257d8d3fc
OUSSAMA NAYA <oussamanaya8@gmail.com>
2026-03-26 12:04:35 +0100

    add the 'Exercise 1 : React Error Boundary Simulation' of the 'Exercises XP' of the week 7 day 4

---
+++ geeks_institute_bootcamp/week_7/day4/Exercises XP/vite-project/src/components/BuggyCounter.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+class BuggyCounter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
+
+  handleClick = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + 1
+    }));
+  };
+
+  render() {
+    if (this.state.counter === 5) {
+      throw new Error("I crashed!");
+    }
+
+    return (
+      <h1 onClick={this.handleClick} style={{ cursor: "pointer" }}>
+        {this.state.counter}
+      </h1>
+    );
+  }
+}
+
+export default BuggyCounter;
